5126970387057cc0a8c52eb0be2883c90d4b10e2
vamsi2792 <60707566+vamsi2792@users.noreply.github.com>
2022-11-21 16:18:37 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,13 @@
-
+def occurance(ch, st):
+	if (len(st) == 0):
+		return 0
+	count = 0
+	if (st[0] == ch):
+		count += 1
+	count += occurance(ch, st[1:])
+	return count
+	
+str1 = input()
+str2=input()
+c = str2[-1]
+print(occurance(c, str1))
